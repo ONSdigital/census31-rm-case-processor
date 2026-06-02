602909f84fd034b43fd6a692046b6bece8982222
ryangrundy7 <35301276+ryangrundy7@users.noreply.github.com>
2026-06-02 15:35:59 +0100

    CN-39 reintroduce QID types (#5)

--- src/main/java/uk/gov/ons/census/caseprocessor/cache/UacQidCache.java
+++ src/main/java/uk/gov/ons/census/caseprocessor/cache/UacQidCache.java
@@ -1,10 +1,15 @@
 package uk.gov.ons.census.caseprocessor.cache;
 
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.support.TransactionSynchronization;
@@ -14,6 +19,9 @@ import uk.gov.ons.census.caseprocessor.model.dto.UacQidDTO;
 
 @Component
 public class UacQidCache {
+
+  private static final Logger log = LoggerFactory.getLogger(UacQidCache.class);
+
   private final UacQidServiceClient uacQidServiceClient;
 
   @Value("${uacservice.uacqid-cache-min}")
@@ -27,24 +35,28 @@ public class UacQidCache {
 
   private static final Executor executor = Executors.newFixedThreadPool(8);
 
-  private BlockingQueue<UacQidDTO> uacQidLinkCache = new LinkedBlockingDeque<>();
-  private boolean isToppingUpCache = false;
-  private final Object lock = new Object();
+  private final Map<Integer, BlockingQueue<UacQidDTO>> uacQidLinkQueueMap =
+      new ConcurrentHashMap<>();
+  private final Set<Integer> isToppingUpQueue = ConcurrentHashMap.newKeySet();
 
   public UacQidCache(UacQidServiceClient uacQidServiceClient) {
     this.uacQidServiceClient = uacQidServiceClient;
   }
 
-  public UacQidDTO getUacQidPair() {
+  public UacQidDTO getUacQidPair(int questionnaireType) {
+    uacQidLinkQueueMap.computeIfAbsent(questionnaireType, key -> new LinkedBlockingDeque<>());
+
     try {
-      topUpCache();
-      UacQidDTO uacQidDTO = uacQidLinkCache.poll(uacQidGetTimout, TimeUnit.SECONDS);
+      topUpQueue(questionnaireType);
+      UacQidDTO uacQidDTO =
+          uacQidLinkQueueMap.get(questionnaireType).poll(uacQidGetTimout, TimeUnit.SECONDS);
 
       if (uacQidDTO == null) {
         // The cache topper upper is executed in a separate thread, which can fail if uacqid api
         // down
         // So check we get a non null result otherwise throw a RunTimeException to re-enqueue msg
-        throw new RuntimeException("Timeout getting UacQidDTO");
+        throw new RuntimeException(
+            "Timeout getting UacQidDTO for questionnaireType :" + questionnaireType);
       }
 
       // Put the UAC-QID back into the cache if the transaction rolls back
@@ -54,7 +66,7 @@ public class UacQidCache {
               @Override
               public void afterCompletion(int status) {
                 if (status == STATUS_ROLLED_BACK) {
-                  uacQidLinkCache.add(uacQidDTO);
+                  uacQidLinkQueueMap.get(questionnaireType).add(uacQidDTO);
                 }
               }
             });
@@ -66,23 +78,29 @@ public class UacQidCache {
     }
   }
 
-  private void topUpCache() {
-    // We use synchronised on an empty object instead of the isToppingUpCache bool because it's
-    // bad practice to use it on a boolean literal.
-    synchronized (lock) {
-      if (!isToppingUpCache && uacQidLinkCache.size() < cacheMin) {
-        isToppingUpCache = true;
+  private void topUpQueue(int questionnaireType) {
+    synchronized (isToppingUpQueue) {
+      if (!isToppingUpQueue.contains(questionnaireType)
+          && uacQidLinkQueueMap.get(questionnaireType).size() < cacheMin) {
+        isToppingUpQueue.add(questionnaireType);
       } else {
         return;
       }
-      executor.execute(
-          () -> {
-            try {
-              uacQidLinkCache.addAll(uacQidServiceClient.getUacQids(cacheFetch));
-            } finally {
-              isToppingUpCache = false;
-            }
-          });
     }
+    log.atInfo()
+        .setMessage("Topping up UAC-QID cache")
+        .addKeyValue("questionnaireType", questionnaireType)
+        .log();
+
+    executor.execute(
+        () -> {
+          try {
+            uacQidLinkQueueMap
+                .get(questionnaireType)
+                .addAll(uacQidServiceClient.getUacQids(questionnaireType, cacheFetch));
+          } finally {
+            isToppingUpQueue.remove(questionnaireType);
+          }
+        });
   }
 }
--- src/test/java/uk/gov/ons/census/caseprocessor/cache/UacQidCacheTest.java
+++ src/test/java/uk/gov/ons/census/caseprocessor/cache/UacQidCacheTest.java
@@ -38,9 +38,9 @@ public class UacQidCacheTest {
     uacQidDTO1.setQid("54321");
     listUacDtos.add(uacQidDTO2);
 
-    when(uacQidServiceClient.getUacQids(2)).thenReturn(listUacDtos);
+    when(uacQidServiceClient.getUacQids(1, 2)).thenReturn(listUacDtos);
 
-    UacQidDTO actualUacDto = underTest.getUacQidPair();
+    UacQidDTO actualUacDto = underTest.getUacQidPair(1);
     assertThat(actualUacDto.getQid()).isEqualTo(uacQidDTO1.getQid());
   }
 
@@ -52,10 +52,12 @@ public class UacQidCacheTest {
 
     List<UacQidDTO> listUacDtos = new ArrayList<>();
 
-    when(uacQidServiceClient.getUacQids(2)).thenReturn(listUacDtos);
+    when(uacQidServiceClient.getUacQids(1, 2)).thenReturn(listUacDtos);
 
-    RuntimeException thrown = assertThrows(RuntimeException.class, () -> underTest.getUacQidPair());
+    RuntimeException thrown =
+        assertThrows(RuntimeException.class, () -> underTest.getUacQidPair(1));
 
-    Assertions.assertThat(thrown.getMessage()).isEqualTo("Timeout getting UacQidDTO");
+    Assertions.assertThat(thrown.getMessage())
+        .isEqualTo("Timeout getting UacQidDTO for questionnaireType :1");
   }
 }
